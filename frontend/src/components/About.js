eb3fa27180d2e3dc8f5ee464eed613d3ae37acbb
VinyasSahai02 <142114422+VinyasSahai02@users.noreply.github.com>
2023-12-23 01:19:34 +0530

    Add files via upload

---
+++ frontend/src/components/About.js
@@ -0,0 +1,17 @@
+import { Container } from "@mui/material";
+import React from "react";
+import '../components/about.css'
+
+
+const About = () => {
+  return (
+    <div>
+      <Container>
+      
+        
+      </Container>
+    </div>
+  );
+};
+
+export default About;
